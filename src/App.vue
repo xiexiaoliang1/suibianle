7d896d7e88828410c1a1e0c97086d9808b3427ac
xxl <291426128@qq.com>
2019-08-28 14:55:34 +0800

    新建

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">孙林</router-link> |
+      <router-link to="/daizanwen">戴赞文</router-link> |
+	  <router-link to="/xiexiaoliang">谢小梁</router-link>
     </div>
     <router-view/>
   </div>
--- src/router.js
+++ src/router.js
@@ -14,12 +14,20 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/daizanwen',
+      name: 'daizanwen',
       // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
+      // this generates a separate chunk (daizanwen.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+      component: () => import(/* webpackChunkName: "daizanwen" */ './views/daizanwen.vue')
+    },
+	{
+	  path: '/xiexiaoliang',
+	  name: 'xiexiaoliang',
+	  // route level code-splitting
+	  // this generates a separate chunk (xiexiaoliang.[hash].js) for this route
+	  // which is lazy-loaded when the route is visited.
+	  component: () => import(/* webpackChunkName: "xiexiaoliang" */ './views/xiexiaoliang.vue')
+	}
   ]
 })
