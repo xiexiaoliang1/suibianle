36b2eaba1863ef240a91c4d8fcd44ec628ac62eb
xxl <291426128@qq.com>
2019-08-28 14:56:37 +0800

    新建的

---
+++ src/views/daizanwen.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>戴赞文的封装</h1>
+  </div>
+</template>
---
+++ src/views/xiexiaoliang.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		谢小梁封装
+	</div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
